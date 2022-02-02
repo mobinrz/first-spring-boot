aca70ccf9a06339f8a040638f2de891b4be31d93
mobin-newroz <ahmobin@newroztech.com>
2022-02-02 16:30:20 +0600

    get api created for string and json output

--- src/main/java/com/example/firstsprintbootproject/controller/HelloController.java
+++ src/main/java/com/example/firstsprintbootproject/controller/HelloController.java
@@ -1,7 +1,11 @@
 package com.example.firstsprintbootproject.controller;
 
+import com.example.firstsprintbootproject.service.Topic;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class HelloController {
@@ -10,8 +14,11 @@ public class HelloController {
         return "HELLO";
     }
 
-    @GetMapping("/hi")
-    public String sayHi(){
-        return "HI";
+    @GetMapping("/topics")
+    public List<Topic> topicList(){
+        return Arrays.asList(
+                new Topic("Java","Java Language","Java Language Description"),
+                new Topic("Spring","Spring Framework","Spring Framework Description")
+        );
     }
 }
