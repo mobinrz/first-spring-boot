a1132bde2fb40811f5f58d9906ab3a60c22e7d00
mobin-newroz <ahmobin@newroztech.com>
2022-02-02 16:16:45 +0600

    first java spring boot project

---
+++ src/main/java/com/example/firstsprintbootproject/FirstSprintBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstsprintbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSprintBootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSprintBootProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/firstsprintbootproject/FirstSprintBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstsprintbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSprintBootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
